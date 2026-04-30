c64d4cb92e3767329589633b1dec14065922a554
AureliousKnight <srinikethreddi@gmail.com>
2026-04-30 19:36:05 +0530

    Add files via upload

---
+++ Physics Test_1.py
@@ -0,0 +1,18 @@
+import math
+m = 50000
+a = 45
+F = m * a
+print(F)
+if F < 50000:
+    print("Shit")
+else:
+    print("No shit")
+if F > 38900:
+    print("Good")
+else: print("Horrible")
+A = 5
+B = 7
+sum_of_squares = (A**2 + B**2)
+C = math.sqrt(sum_of_squares)
+print(C)
+
