df51d0067169a2a5d6fe2d0ec55b1b6cad64762e
Sriniketh <srinikethreddi@gmail.com>
2026-05-01 19:07:44 +0530

    Add files via upload

---
+++ Mathematics/Trigonometry_Calculations.py
@@ -0,0 +1,13 @@
+import math
+A = math.sin(math.pi/2)
+B = math.cos(math.pi*5/3)
+C = A*B
+D = C*math.tan(math.pi*18/10)
+E = C*D - A 
+print("The value of A is :", A)
+print("The value of B:", B)
+print("The vale of C:", C)
+print("The value of D is :", D)
+print("The value of E is :", E)
+print("The total value of A*B*C*D*E is :", A*B*C*D*E)
+
