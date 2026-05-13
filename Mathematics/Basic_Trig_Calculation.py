f2f7b2dfebd5c9664f3d82daf9c7ee51a212bfb1
Sriniketh <srinikethreddi@gmail.com>
2026-05-13 23:41:02 +0530

    Add files via upload
    This code is pretty basic but after a break, I just wanted to ease into it,
    thanks for viewing

---
+++ Mathematics/Basic_Trig_Calculation.py
@@ -0,0 +1,16 @@
+import math
+x = math.pi/55
+A = math.cos(4*x) + math.cos(3*x) + math.cos(2*x)
+B = math.sin(3*x) + math.sin(2*x) + math.sin(2*x)
+C = A**2 + B**2
+D = math.sqrt(C)
+E = C**D
+F = E/math.pi
+S = E/F**67
+print(A)
+print(B)
+print(C)    
+print(D)
+print(E)
+print(F)
+print(S)
